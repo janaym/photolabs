5c241478e396125e670d367dd8d30a7fbacab684
Janay Ma <janay.ma7@gmail.com>
2023-09-15 20:49:35 -0700

    move home route to routes folder from components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import HomeRoute from 'components/HomeRoute';
+import HomeRoute from 'routes/HomeRoute';
 
 import './App.scss';
 
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import TopNavigationBar from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
+
+
+
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
+const HomeRoute = (props) => {
+
+  const { GlobalState, photos, topics } = props;
   return (
-    <div className="home-route">
-      {/* Insert React */}
+    <div className='home-route'>
+      <TopNavigationBar topics={topics} GlobalState={GlobalState} />
+      <PhotoList photos={photos} GlobalState={GlobalState} />
     </div>
   );
 };
